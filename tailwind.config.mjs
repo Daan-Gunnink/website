3e3daea75c2302d5d2d9dfb863faaf398481c3cb
Daan Gunnink <github@kreukelz.one>
2024-12-30 17:24:14 +0100

    Initial website

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			anton: ['Anton', 'sans-serif'],
+			inter: ['Inter', 'sans-serif'],
+		},
+		extend: {},
+	},
+	plugins: [],
+}
