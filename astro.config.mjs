6f89740b3fb2f3cbddb509c1fe69faf1c4518b06
Daan Gunnink <github@kreukelz.one>
2025-01-02 15:08:35 +0100

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://daan-gunnink.github.io',
+  base: 'website',
   integrations: [tailwind(), mdx()],
   experimental: {
     svg: true,
