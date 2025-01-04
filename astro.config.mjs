27b8b1dc010031b54725dbcabb7dfb5bc90e56c2
Daan Gunnink <github@kreukelz.one>
2025-01-04 22:28:11 +0100

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://daan.sh',
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), sitemap()],
   experimental: {
     svg: true,
   }
